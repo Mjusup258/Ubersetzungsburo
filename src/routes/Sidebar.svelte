b3a63ed7f6e003445590de3a1827985f8b0e3161
Gustav Natale <gustav.natale@gmail.com>
2024-07-27 15:23:05 +0200

    I added the logo and text underneath

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -5,7 +5,19 @@
 
 <aside class="absolute w-full h-full bg-gray-200 border-r-2 shadow-lg" class:open>
     <nav class="p-12 text-xl">
-        <img src="./static/favicon.png"/>
+        <div class="logo-container">
+            <img src="./static/favicon.png" alt="Stanic Logo" class="img-logo"/>
+            <div class="img-text">
+                <b id="name">Vlado Stanić</b><br>
+                Staatlich geprüfter Übersetzer für die <br>
+                bosnische, kroatische und serbische Sprache <br>
+                Friedenstr. 16, D-90765 Fürth <br>
+                vlado.stanic@vodafonemail.de <br>
+                Tel: +49 911 89293933 <br>
+                Fax: +49 911 25356400 <br>
+                Mob: +49 171 2971506
+            </div>
+        </div>
         <a class="block" href="#main"><Icon icon="material-symbols-light:home" />Hauptseite</a>
         <a class="block" href="#service"><Icon icon="material-symbols-light:account-tree" />Service</a>
         <a class="block" href="#about"><Icon icon="material-symbols-light:frame-person-sharp" />Über mich</a>
@@ -24,6 +36,19 @@
     .open {
         left: 75%;
     }
+    .img-text {
+        text-align: center;
+        margin-top: 10px;
+        font-size: 0.32cm;
+        color: black;
+        line-height: 15px;
+    }
+    .logo-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
     @media (max-width: 768px) {
         aside {
             width: 100%;
@@ -31,6 +56,12 @@
 
         .open {
             left: 0;
+        }
+        .img-text {
+            font-size: 0.4cm; /* Increase font size for mobile devices */
+        }
+        .img-logo{
+            max-width: 200px;
         }
     }
 </style>
