1daee2e92676d4ac9d5f8dc481eb60ba3e5c58f8
Gustav Natale <gustav.natale@gmail.com>
2024-07-27 22:56:27 +0200

    Working Router to Impressum

---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Navbar from './Navbar.svelte'
+    import Sidebar from './Sidebar.svelte'
+
+    let open = true
+</script>
+
+<title>Übersetzungsbüro Vlado Stanić</title>
+<Sidebar bind:open/>
+<Navbar bind:sidebar={open}/>
+
+
+<svelte:head>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
+</svelte:head>
+
+<style>
+    :global(body) {
+        padding: 0;
+    }
+</style>
